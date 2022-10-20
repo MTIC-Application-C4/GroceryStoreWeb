d5ca7f6438c42af856e66119e8a0fec28ccbb041
MaxPoer007-Rs <ing.control.adrian@gmail.com>
2022-10-20 16:02:29 -0500

    first commit

--- js/script.js
+++ js/script.js
@@ -2,22 +2,41 @@ let searchForm = document.querySelector('.search-form');
 
 document.querySelector('#search-btn').onclick = () => {
     searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    loginForm.classList.remove('active');
+    shoppingCart.classList.remove('active');    
 }
 
 let shoppingCart = document.querySelector('.shopping-cart');
 
 document.querySelector('#cart-btn').onclick = () => {
     shoppingCart.classList.toggle('active');
+    navbar.classList.remove('active');
+    loginForm.classList.remove('active');
+    searchForm.classList.toggle('active');
 }
 
 let loginForm = document.querySelector('.login-form');
 
 document.querySelector('#login-btn').onclick = () => {
     loginForm.classList.toggle('active');
+    navbar.classList.remove('active');    
+    shoppingCart.classList.remove('active');
+    searchForm.classList.toggle('active');
 }
 
 let navbar = document.querySelector('.navbar');
 
 document.querySelector('#menu-btn').onclick = () => {
-    navbar.classList.toggle('active');
+    navbar.classList.toggle('active');    
+    loginForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    searchForm.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    loginForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    searchForm.classList.toggle('active');
 }
